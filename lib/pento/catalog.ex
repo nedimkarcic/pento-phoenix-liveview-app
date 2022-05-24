3e4b15140b398862a4ba88661bffccf13dea4827
Nedim <nedimkarcic52@gmail.com>
2022-05-24 12:12:07 +0200

    Add markdown_product context function

--- lib/pento/catalog.ex
+++ lib/pento/catalog.ex
@@ -101,4 +101,11 @@ defmodule Pento.Catalog do
   def change_product(%Product{} = product, attrs \\ %{}) do
     Product.changeset(product, attrs)
   end
+
+  def markdown_product(%Product{} = product, oper, amount \\ 0) do
+    product
+    |> Product.changeset()
+    |> Product.price_changeset(oper, amount)
+    |> Repo.update()
+  end
 end
