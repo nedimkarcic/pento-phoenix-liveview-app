b9c379bbc67cc8ae8a77ee33d5d3567aab90ab2e
Nedim <nedimkarcic52@gmail.com>
2022-08-19 12:02:58 +0200

    Delete unused function

--- lib/pento/accounts.ex
+++ lib/pento/accounts.ex
@@ -12,35 +12,31 @@ defmodule Pento.Accounts do
 
   @doc """
   Gets a user by email.
+
   ## Examples
+
       iex> get_user_by_email("foo@example.com")
       %User{}
+
       iex> get_user_by_email("unknown@example.com")
       nil
+
   """
   def get_user_by_email(email) when is_binary(email) do
     Repo.get_by(User, email: email)
   end
 
   @doc """
-  Gets a user by username.
-  ## Examples
-      iex> get_user_by_username("foo")
-      %User{}
-      iex> get_user_by_username("unknown")
-      nil
-  """
-  def get_user_by_username(username) when is_binary(username) do
-    Repo.get_by(User, username: username)
-  end
-
-  @doc """
   Gets a user by email and password.
+
   ## Examples
+
       iex> get_user_by_email_and_password("foo@example.com", "correct_password")
       %User{}
+
       iex> get_user_by_email_and_password("foo@example.com", "invalid_password")
       nil
+
   """
   def get_user_by_email_and_password(email, password)
       when is_binary(email) and is_binary(password) do
@@ -50,12 +46,17 @@ defmodule Pento.Accounts do
 
   @doc """
   Gets a single user.
+
   Raises `Ecto.NoResultsError` if the User does not exist.
+
   ## Examples
+
       iex> get_user!(123)
       %User{}
+
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
+
   """
   def get_user!(id), do: Repo.get!(User, id)
 
@@ -63,11 +64,15 @@ defmodule Pento.Accounts do
 
   @doc """
   Registers a user.
+
   ## Examples
+
       iex> register_user(%{field: value})
       {:ok, %User{}}
+
       iex> register_user(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
+
   """
   def register_user(attrs) do
     %User{}
@@ -77,9 +82,12 @@ defmodule Pento.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
+
   ## Examples
+
       iex> change_user_registration(user)
       %Ecto.Changeset{data: %User{}}
+
   """
   def change_user_registration(%User{} = user, attrs \\ %{}) do
     User.registration_changeset(user, attrs, hash_password: false)
@@ -87,32 +95,14 @@ defmodule Pento.Accounts do
 
   ## Settings
 
-  ### username
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for changing the user username.
-  ## Examples
-      iex> change_user_username(user)
-      %Ecto.Changeset{data: %User{}}
-  """
-  def change_user_username(user, attrs \\ %{}) do
-    User.username_changeset(user, attrs)
-  end
-
-  def update_user_username(user, password, attrs) do
-    user
-    |> User.username_changeset(attrs)
-    |> User.validate_current_password(password)
-    |> Repo.update
-  end
-
-  ### email
-
   @doc """
   Returns an `%Ecto.Changeset{}` for changing the user email.
+
   ## Examples
+
       iex> change_user_email(user)
       %Ecto.Changeset{data: %User{}}
+
   """
   def change_user_email(user, attrs \\ %{}) do
     User.email_changeset(user, attrs)
@@ -121,11 +111,15 @@ defmodule Pento.Accounts do
   @doc """
   Emulates that the email will change without actually changing
   it in the database.
+
   ## Examples
+
       iex> apply_user_email(user, "valid password", %{email: ...})
       {:ok, %User{}}
+
       iex> apply_user_email(user, "invalid password", %{email: ...})
       {:error, %Ecto.Changeset{}}
+
   """
   def apply_user_email(user, password, attrs) do
     user
@@ -136,6 +130,7 @@ defmodule Pento.Accounts do
 
   @doc """
   Updates the user email using the given token.
+
   If the token matches, the user email is updated and the token is deleted.
   The confirmed_at date is also updated to the current time.
   """
@@ -161,9 +156,12 @@ defmodule Pento.Accounts do
 
   @doc """
   Delivers the update email instructions to the given user.
+
   ## Examples
+
       iex> deliver_update_email_instructions(user, current_email, &Routes.user_update_email_url(conn, :edit, &1))
       {:ok, %{to: ..., body: ...}}
+
   """
   def deliver_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
       when is_function(update_email_url_fun, 1) do
@@ -175,9 +173,12 @@ defmodule Pento.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for changing the user password.
+
   ## Examples
+
       iex> change_user_password(user)
       %Ecto.Changeset{data: %User{}}
+
   """
   def change_user_password(user, attrs \\ %{}) do
     User.password_changeset(user, attrs, hash_password: false)
@@ -185,11 +186,15 @@ defmodule Pento.Accounts do
 
   @doc """
   Updates the user password.
+
   ## Examples
+
       iex> update_user_password(user, "valid password", %{password: ...})
       {:ok, %User{}}
+
       iex> update_user_password(user, "invalid password", %{password: ...})
       {:error, %Ecto.Changeset{}}
+
   """
   def update_user_password(user, password, attrs) do
     changeset =
@@ -238,11 +243,15 @@ defmodule Pento.Accounts do
 
   @doc """
   Delivers the confirmation email instructions to the given user.
+
   ## Examples
+
       iex> deliver_user_confirmation_instructions(user, &Routes.user_confirmation_url(conn, :edit, &1))
       {:ok, %{to: ..., body: ...}}
+
       iex> deliver_user_confirmation_instructions(confirmed_user, &Routes.user_confirmation_url(conn, :edit, &1))
       {:error, :already_confirmed}
+
   """
   def deliver_user_confirmation_instructions(%User{} = user, confirmation_url_fun)
       when is_function(confirmation_url_fun, 1) do
@@ -257,6 +266,7 @@ defmodule Pento.Accounts do
 
   @doc """
   Confirms a user by the given token.
+
   If the token matches, the user account is marked as confirmed
   and the token is deleted.
   """
@@ -280,9 +290,12 @@ defmodule Pento.Accounts do
 
   @doc """
   Delivers the reset password email to the given user.
+
   ## Examples
+
       iex> deliver_user_reset_password_instructions(user, &Routes.user_reset_password_url(conn, :edit, &1))
       {:ok, %{to: ..., body: ...}}
+
   """
   def deliver_user_reset_password_instructions(%User{} = user, reset_password_url_fun)
       when is_function(reset_password_url_fun, 1) do
@@ -293,11 +306,15 @@ defmodule Pento.Accounts do
 
   @doc """
   Gets the user by reset password token.
+
   ## Examples
+
       iex> get_user_by_reset_password_token("validtoken")
       %User{}
+
       iex> get_user_by_reset_password_token("invalidtoken")
       nil
+
   """
   def get_user_by_reset_password_token(token) do
     with {:ok, query} <- UserToken.verify_email_token_query(token, "reset_password"),
@@ -310,11 +327,15 @@ defmodule Pento.Accounts do
 
   @doc """
   Resets the user password.
+
   ## Examples
+
       iex> reset_user_password(user, %{password: "new long password", password_confirmation: "new long password"})
       {:ok, %User{}}
+
       iex> reset_user_password(user, %{password: "valid", password_confirmation: "not the same"})
       {:error, %Ecto.Changeset{}}
+
   """
   def reset_user_password(user, attrs) do
     Ecto.Multi.new()
